c97265040a30d5cfd1769c35dd3394ddbcb1b0c3
Zayleth <zaylethvegas.com>
2024-05-24 01:04:40 -0400

    UpperCase, LowerCase and replace exercises (5)

---
+++ Java Script/String_Methods.js
@@ -0,0 +1,35 @@
+// String Methods
+// EXERCISES
+
+// 1. Convert the text into an UPPERCASE text:
+
+let text1 = "Hello World!";       // String
+let text2 = text1.toUpperCase();  // text2 is text1 converted to lower
+
+
+
+// 2. Use the slice method to return the word "bananas".
+
+let txt = "I can eat bananas all day";
+let x = txt.slice(10, 17); // bananas
+
+
+
+// 3. Use the correct String method to replace the word "Hello" with the word "Welcome".
+
+let txt_1 = "Hello World";
+txt = txt_1.replace("Hello", "Welcome");
+
+
+
+// 4. Convert the value of txt to upper case.
+
+let txt_2 = "Hello World";
+txt = txt_2.toUpperCase();
+
+
+
+// 5. Convert the value of txt to lower case.
+
+let txt_3 = "Hello World";
+txt = txt_3.toLowerCase();
