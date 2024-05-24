9e404f9fffabd0fc3fda40a80793f5d5182d7cea
Zayleth <zaylethvegas.com>
2024-05-23 22:10:23 -0400

    Backticks - “extended functionality” quotes. (1)

---
+++ Java Script/Data_Types.js
@@ -0,0 +1,10 @@
+// TASKS 
+// String quotes. What is the output of the script?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // answer: hello 1
+
+alert( `hello ${"name"}` ); // answer: hello name
+
+alert( `hello ${name}` ); // answer: hello Ilya
