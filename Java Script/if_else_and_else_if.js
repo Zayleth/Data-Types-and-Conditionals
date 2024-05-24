0de8458a10b9af9c70f0c2c3f5234e9567a2251e
Zayleth <zaylethvegas.com>
2024-05-24 15:36:16 -0400

    Conditionals exercises - if, else (2)

---
+++ Java Script/if_else_and_else_if.js
@@ -0,0 +1,25 @@
+// Conditional statements are used to perform different actions based on different conditions.
+/*
+
+- Use if to specify a block of code to be executed, if a specified condition is true
+- Use else to specify a block of code to be executed, if the same condition is false
+- Use else if to specify a new condition to test, if the first condition is false
+- Use switch to specify many alternative blocks of code to be executed
+
+*/
+// Fix the if statement to alert "Hello World" if x is greater than y.
+
+if (x > y) {
+    alert("Hello World");
+}
+
+
+
+// Fix the if statement to alert "Hello World" if x is greater than y, otherwise alert "Goodbye".
+
+if (x > y) {
+    alert("Hello World");
+
+} else {
+    alert("Goodbye");
+}
